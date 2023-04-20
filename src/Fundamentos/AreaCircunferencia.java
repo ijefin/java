e215b830de9b1936bd3278e00b4849752f1e2eb0
ijefin <jeffersonmecmiler@hotmail.com>
2023-04-20 16:26:14 -0300

    first java commit

---
+++ src/Fundamentos/AreaCircunferencia.java
@@ -0,0 +1,13 @@
+package Fundamentos;
+
+public class AreaCircunferencia {
+    public static void main(String[] args) {
+
+        double raio = 5.6;
+        final double PI = 3.14;
+
+        double result = raio * PI;
+
+        System.out.println(result);
+    }
+}
---
+++ src/Fundamentos/FarenheitCelsius.java
@@ -0,0 +1,10 @@
+package Fundamentos;
+
+public class FarenheitCelsius {
+    public static void main(String[] args) {
+        double farenheit = 80;
+        final double result = (farenheit - 32) * 5/9;
+
+        System.out.println(result);
+    }
+}
